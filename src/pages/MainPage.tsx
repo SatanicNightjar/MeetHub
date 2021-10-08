fa1579098f1ddfbe8e00dc48cc3c7dee82864a29
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-10-08 14:43:25 +0200

    Remove unused imports

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Heading, HStack, Icon, Stack, Text } from '@chakra-ui/react';
+import { Box, Divider, Heading, Text } from '@chakra-ui/react';
 import { DocumentData } from 'firebase/firestore';
 import React from 'react';
 import { useEffect } from 'react';
